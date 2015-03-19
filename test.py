970761c6370e5cd9da4c2934aa7cb3c95ffa9241
linqing-lu <lulinqing@foxmail.com>
2015-03-19 21:31:35 +0800

    取得重大进展

--- test.py
+++ test.py
@@ -11,7 +11,9 @@ import time
 from pymongo import MongoClient
 
 class Duobao:
-	baseurl = "http://1.163.com/history/01-48-00-18-33.html"
+	filter1 = "\?cid="
+	filter2 = "index.do\?cid="
+	baseurl = "http://1.163.com/user/index.do?cid=41022423"
 	urls = []
 	def getdata(self):
 		url = str(self.baseurl)
@@ -25,7 +27,7 @@ class Duobao:
 		soup = BeautifulSoup(html, "lxml")
 		# print(soup.prettify())
 		# print(soup.find_all('a'))
-		links = soup.find_all('a', href=re.compile('detail/01-48-'))
+		links = soup.find_all('a', href=re.compile(self.filter1))
 		for link in links:
 			addr = link.get('href')
 			if self.checkUrl(addr):
@@ -38,7 +40,7 @@ class Duobao:
 			
 			# print(addr)
 	def getdatas(self, url):
-		self.crawData(url, 'body')
+		self.crawData(url, 'code')
 		res = requests.get(url)
 		html = res.content
 		try:
@@ -49,7 +51,7 @@ class Duobao:
 		soup = BeautifulSoup(html, "lxml")
 		# print(soup.prettify())
 		# print(soup.find_all('a'))
-		links = soup.find_all('a', href=re.compile('detail/01-48-'))
+		links = soup.find_all('a', href=re.compile(self.filter2))
 		for link in links:
 			addr = link.get('href')
 			if self.checkUrl(addr):
@@ -61,8 +63,8 @@ class Duobao:
 				self.getdatas(addr)
 			
 	def checkUrl(self, url):
-		print self.urls
-		print url
+		# print self.urls
+		# print url
 		for turl in self.urls:
 			if turl == url:
 				return True
@@ -76,13 +78,36 @@ class Duobao:
 	def crawData(self, url, tag_name):
 		driver = webdriver.PhantomJS()
 		driver.get(url);
-		# time.sleep(2)
-		data = driver.find_element_by_tag_name(tag_name)
-		print data.text
+		data = driver.find_elements_by_tag_name('table')
+		for dt in data:
+			print dt.text
+			print "====================================="
 		driver.quit()
-						
+
+	def crawData2(self, url, tag_name):
+		driver = webdriver.PhantomJS()
+		driver.get(url);
+		# time.sleep(2)
+		# data = driver.find_element_by_tag_name(tag_name)
+		data = driver.find_elements_by_class_name('code')
+		for dt in data:
+			print dt.text
+		data2 = driver.find_elements_by_class_name('winner')
+		for dt2 in data2:
+			print dt2.text
+		driver.quit()
+		
+	def crawData3(self, url, tag_name):
+		driver = webdriver.PhantomJS()
+		driver.get(url);
+		data = driver.find_elements_by_tag_name('table')
+		for dt in data:
+			print dt.text
+			print "====================================="
+		driver.quit()
 	def __unicode__(self):
 		return self.baseurl
 
 db = Duobao()
-db.getdata()
+# db.getdata()
+db.crawData(db.baseurl, 'table')
