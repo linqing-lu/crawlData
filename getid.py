edff4939cf5710846049904841776a9be19b371c
linqing-lu <lulinqing@foxmail.com>
2015-03-22 12:55:35 +0800

    代码优化

--- getid.py
+++ getid.py
@@ -56,6 +56,9 @@ class Fetcher(object):
 		# 	if turl == url:
 		# 		return True
 		# return False
+		temp = re.findall(r'cid=(\w*[0-9]+)\w*', winner_url)
+		if int(temp[0]) == int(cid):
+			return True
 		if self.local_db.urls.find({"_id": url}).count() > 0:
 			return True
 		else:
