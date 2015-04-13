d50dbe5fbdd1ef1b521da56b95e0c673da03f3ab
lulinqing <lulinqing@foxmail.com>
2015-04-13 09:18:25 +0800

    优化代码

--- getgoods.py
+++ getgoods.py
@@ -25,42 +25,70 @@ class GoodsInfo(object):
 		self.dbclient = MongoClient('mongodb://198.52.117.75', 27017)
 		self.db = self.dbclient.duobao
 		self.db.authenticate('test', 'duobao')
-	def __unicode__(self):
-		return self.goods_id
-	def fetchOne(self, period):
-		session = requests.Session()
+		self.session = requests.Session()
 		r = session.get('http://1.163.com',headers=post_headers)
 		if r.status_code == 200:
 			cookie = r.headers['set-cookie']
 			strs = cookie.split(';')
 			otoken = strs[0]
-			token = otoken.split('=')[1]
-			info = {'gid':self.goods_id,'period':period,'t':1428847870372,'token':token}
-			res = session.get(base_url, params=info)
-			if res.status_code == 200:
-				result = res.json()
-				result = result['result']['periodWinner']
-				detail = {}
-				m = hashlib.md5()
-				m.update(result['goods']['gname'].encode('utf8'))
-				detail['_id'] = str("%s_%d") % (m.hexdigest(), period)
-				detail['title'] = result['goods']['gname']
-				detail['price'] = result['goods']['price']
-				detail['issue'] = period
-				detail['cost'] = result['ownerCost']
-				detail['winner_cid'] = result['owner']['cid']
-				detail['winner_uid'] = result['owner']['uid']
-				detail['ip_address'] = result['owner']['IPAddress']
-				detail['lucky_code'] = result['luckyCode']
-				print period
-				self.saveToMongoDB(detail, self.db.detail)
-			else:
-				print res.status_code
+			self.token = otoken.split('=')[1]
+			print 'create session success'
+		else:
+			print 'create session failed'
+	def uninit(self):
+		self.db.logout()
+		self.dbclient.close()
+	def __unicode__(self):
+		return self.goods_id
+	def fetchOne(self, period):
+		info = {'gid':self.goods_id,'period':period,'t':1428847870372,'token':self.token}
+		res = self.session.get(base_url, params=info)
+		if res.status_code == 200:
+			result = res.json()
+			result = result['result']['periodWinner']
+			detail = {}
+			# m = hashlib.md5()
+			# m.update(result['goods']['gname'].encode('utf8'))
+			detail['_id'] = str("%d_%d") % (self.goods_id, period)
+			detail['title'] = result['goods']['gname']
+			detail['price'] = result['goods']['price']
+			detail['issue'] = period
+			detail['cost'] = result['ownerCost']
+			detail['winner_cid'] = result['owner']['cid']
+			detail['winner_uid'] = result['owner']['uid']
+			detail['ip_address'] = result['owner']['IPAddress']
+			detail['lucky_code'] = result['luckyCode']
+			detail['gid'] = self.goods_id
+			print detail['_id']
+			self.saveToMongoDB(detail, self.db.detail)
+			return True
+		else:
+			print res.status_code
+			return False
+	def getMaxIssue(self):
+		data = self.db.detail.find({"gid":self.goods_id}).sort([("issue", -1)]).limit(1)
+		maxIssue = data[0]['issue']
+		return int(maxIssue) + 1
 	def saveToMongoDB(self, data, collection):
-		collection.save(data);
+		collection.save(data)
 def main():
-	g = GoodsInfo(158)
-	for i in range(1, 3000):
-		g.fetchOne(i)
+	for gid in range(1,999):
+		try:
+			g = GoodsInfo(gid)
+			minIssue = 1
+			try:
+				minIssue = g.getMaxIssue()
+			except Exception, e:
+				pass
+			print str("gid=%d minIssue=%d") %(gid, minIssue)
+			for i in range(minIssue, 5000):
+				if g.fetchOne(i):
+					continue
+				else:
+					break
+			g.uninit()
+		except Exception, e:
+			pass
+
 if __name__ == '__main__':
 	main()
--- test.py
+++ test.py
@@ -104,58 +104,58 @@ class Duobao(object):
 			except Exception, e:
 				pass
 
-			winned = False
-			try:
-				col3 = dt.find_element_by_class_name('col3')
-				if col3.text == ("已揭晓").decode('utf8'):
-					winned = True
-				else:
-					continue
-			except Exception, e:
-				continue
+			# winned = False
+			# try:
+			# 	col3 = dt.find_element_by_class_name('col3')
+			# 	if col3.text == ("已揭晓").decode('utf8'):
+			# 		winned = True
+			# 	else:
+			# 		continue
+			# except Exception, e:
+			# 	continue
 
-			if winned:
-				winner_info = {}
-				try:
-					# m = hashlib.md5()
-					# m.update(goods_title.encode('utf8'))
-					winner_info['_id'] = goods_issue
-					winner_info['issue'] = goods_issue
-					winner_info['title'] = goods_title
-					goods_info = self.db.detail.find({"_id": winner_info['_id']})
-					if goods_info != None and goods_info.has_key('lucky_code'):
-						continue
-				except Exception, e:
-					continue
+			# if winned:
+			# 	winner_info = {}
+			# 	try:
+			# 		# m = hashlib.md5()
+			# 		# m.update(goods_title.encode('utf8'))
+			# 		winner_info['_id'] = goods_issue
+			# 		winner_info['issue'] = goods_issue
+			# 		winner_info['title'] = goods_title
+			# 		goods_info = self.db.detail.find({"_id": winner_info['_id']})
+			# 		if goods_info != None and goods_info.has_key('lucky_code'):
+			# 			continue
+			# 	except Exception, e:
+			# 		continue
 
-				try:
-					winner = dt.find_element_by_class_name('winner')
-					# print winner.text
-					winner_name = winner.find_element_by_class_name('name')
-					winner_url = winner_name.find_element_by_tag_name('a').get_attribute('href')
-					temp = re.findall(r'cid=(\w*[0-9]+)\w*', winner_url)
-					winner_cid = int(temp[0])
-					winner_vote = winner_name.find_element_by_class_name('txt-dark')
-					winner_price = int(winner_vote.text)
-					winner_info['cost'] = winner_price
-					winner_info['winner_cid'] = winner_cid
-					winner_code = winner.find_element_by_class_name('code')
-					lucky_code = winner_code.find_elements_by_class_name('txt-impt').text
-					winner_info['lucky_code'] = int(lucky_code)
-					# print winner_price	
-				except Exception, e:
-					continue
+			# 	try:
+			# 		winner = dt.find_element_by_class_name('winner')
+			# 		# print winner.text
+			# 		winner_name = winner.find_element_by_class_name('name')
+			# 		winner_url = winner_name.find_element_by_tag_name('a').get_attribute('href')
+			# 		temp = re.findall(r'cid=(\w*[0-9]+)\w*', winner_url)
+			# 		winner_cid = int(temp[0])
+			# 		winner_vote = winner_name.find_element_by_class_name('txt-dark')
+			# 		winner_price = int(winner_vote.text)
+			# 		winner_info['cost'] = winner_price
+			# 		winner_info['winner_cid'] = winner_cid
+			# 		winner_code = winner.find_element_by_class_name('code')
+			# 		lucky_code = winner_code.find_elements_by_class_name('txt-impt').text
+			# 		winner_info['lucky_code'] = int(lucky_code)
+			# 		# print winner_price	
+			# 	except Exception, e:
+			# 		continue
 
-				try:
-					col2 = dt.find_element_by_class_name('col2')
-					goods_price = col2.find_element_by_class_name('w-goods-price')
-					# print goods_price.text
-					total_price = re.findall(r'(\w*[0-9]+)\w*', goods_price.text)
-					winner_info['price'] = int(total_price[0])
-				except Exception, e:
-					continue
+			# 	try:
+			# 		col2 = dt.find_element_by_class_name('col2')
+			# 		goods_price = col2.find_element_by_class_name('w-goods-price')
+			# 		# print goods_price.text
+			# 		total_price = re.findall(r'(\w*[0-9]+)\w*', goods_price.text)
+			# 		winner_info['price'] = int(total_price[0])
+			# 	except Exception, e:
+			# 		continue
 
-				self.saveToMongoDB(winner_info, self.db.detail)
+			# 	self.saveToMongoDB(winner_info, self.db.detail)
 		print "====================================="
 		print str("self.total_cost = %d") % (self.total_cost)
 		return True
